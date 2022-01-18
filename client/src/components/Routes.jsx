014063c447c54790b1241471e08eae8ceac82640
Harry Jiang <harry10291993@hotmail.com>
2022-01-18 00:57:34 -0500

    Adjust routes

--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import Login from './Login.js';
-import Register from './Register.js';
-import NavBar from './NavBar';
+import Application from './Application.jsx';
+import Login from './Login.jsx';
+import Register from './Register.jsx';
+import NavBar from './NavBar.jsx';
 
 const Routes = () => (
-  <App>
+  <Application>
     <NavBar />
     <Login  exact path="/login" />
     <Register path="/register" />
-  </App>
+  </Application>
 );
 
 export default Routes;
