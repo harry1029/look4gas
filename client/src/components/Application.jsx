90946187d0070c3fb7c926fa53b2b0f3738ffb9a
Harry Jiang <harry10291993@hotmail.com>
2022-01-17 20:42:43 -0500

    Setup react-router and Login route

--- client/src/components/Application.jsx
+++ client/src/components/Application.jsx
@@ -2,13 +2,19 @@ import './Application.scss';
 import useApplicationData from '../hooks/useApplicationData';
 import Navbar from './Navbar';
 
+// Using react-router
+import { Routes, Route, Link } from "react-router-dom";
+
 const Application = () => {
   const {
     state,
     dispatch
   } = useApplicationData();
+
   const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
   ));
+
+  
   return (<div className="Application" >
     <Navbar>Nav</Navbar>
     <h1> Users </h1>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,15 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Set up react router
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
 import Application from "./components/Application";
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Application />
+  </BrowserRouter> , document.getElementById("root")
+);
