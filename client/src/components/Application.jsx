df6f2f7b5b2d766dd8e482b5ea92e093c39426c7
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2022-01-18 13:46:53 -0500

    Design GasPriceItem and modified index.js to show Application.jsx as root, and Home.jsx as index page

--- client/src/components/Application.jsx
+++ client/src/components/Application.jsx
@@ -1,7 +1,7 @@
 import './Application.scss';
 import useApplicationData from '../hooks/useApplicationData';
 import Navbar from './Navbar';
-import Gas_price from './Gas_price';
+import GasPriceItem from './GasPriceItem';
 
 // Using react-router
 import { Routes, Route, Link } from "react-router-dom";
@@ -11,18 +11,14 @@ const Application = () => {
     state,
     dispatch
   } = useApplicationData();
-
   const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
   ));
 
   
   return (<div className="Application" >
-    <Navbar>Nav</Navbar>
-    <div>
+    <Navbar/>
     <h1> Users </h1>
     <ul> {userList} </ul>
-    </div>
-    <Gas_price></Gas_price>
   </div >
   );
 };
